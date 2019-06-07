cc06a51c4f4c3f4b078a37da468495206453ff32
mchalik <maksicmhalik@gmail.com>
2019-06-07 03:20:19 +0300

    start time management

---
+++ Управление временем/index.js
@@ -0,0 +1,36 @@
+/**
+ * @param {String} date
+ * @returns {Object}
+ */
+module.exports = function (date) {
+    var date  = new Date(date);
+    return {
+        subtract : function (value, timeUnit) {
+            var date =
+            return new Date()
+        },
+        add : function (value, timeUnit) {
+            return
+        }
+    }
+
+    function millisecondsFromTimeUnit(timeUnit) {
+        var units = [];
+        for (var i = 0; i <5; i++) {
+            units[i] = 0;
+        }
+
+        if (timeUnit === 'years') units[0]=1;
+        if (timeUnit === 'months') return minute;
+        if (timeUnit === 'days') return minute;
+        if (timeUnit === 'hours') return minute;
+        if (timeUnit === 'minutes') return second;
+
+        years
+        months
+        days
+        hours
+        minutes
+
+    }
+};
