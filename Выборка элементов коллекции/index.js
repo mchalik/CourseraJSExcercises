da77456d8d8de4da66cbce003a6c094bf1ccfbc9
Max Chalik <m.chalik@melstudio.ru>
2019-06-11 21:57:43 +0300

    almost done

--- Выборка элементов коллекции/index.js
+++ Выборка элементов коллекции/index.js
@@ -30,53 +30,101 @@
 // );
 
 
-
+function functionName(func) {
+    var result = /^function\s+([\w$]+)\s*\(/.exec(func.toString());
+    return result[1];
+}
 
 function query(collection) {
-    var collectionCopy = collection.slice();
-    for (var i = 0; i < collectionCopy.length; i++) {
+    var filters = [];
+    var selects = [];
+    var newCollection = collection;
 
-    }
-    var possibleSelections = collectionCopy;
-    var operationSelects = [];
-    var operationFilters = [];
-    for (var i = 1; i < arguments.length; i++) {
-        if (arguments[i].splice) {
-            operationSelects.push(arguments[i]);
-        } else {
-            operationFilters.push(arguments[i]);
+    for (var i = 0; i < arguments.length; i++) {
+        if (functionName(arguments[i]) === 'filterIn') {
+            filters.push(arguments[i]);
+        } else if (func(arguments[i]) === 'select') {
+            selects.push(arguments[i]);
         }
     }
-    for (var i = 0; i < operationSelects; i++) {
-        for (var j = 0; j < collectionCopy.length; j++) {
-            for (var k = 0; k < operationSelects[i]; k++) {
-                var persons = Object.keys(collectionCopy[j]);
-                if (operationSelects[i][k].index)
-            }
-        }
+    for (var i = 0; i < filters.length; i++) {
+        newCollection = filters[i](newCollection);
     }
-
+    for (var i = 0; i < selects.length; i++) {
+        selects[i](newCollection);
+    }
+    return newCollection;
 }
 
 /**
  * @params {String[]}
  */
+
+//     lib.select('name', 'gender', 'email'),
 function select() {
     var acceptedProps = [];
     for (var i = 1; i < arguments.length; i++) {
         acceptedProps.push(arguments[i])
     }
-    return acceptedProps;
+    return function select(collection) {
+        var newCollection = [];
+
+        for (var i = 0; i < collection.length; i++) {
+            var newObject = {};
+
+            var currentCollectionKeys = Object.keys(collection[i]);
+
+            for (var j = 1; j < acceptedProps; j++) {
+                var propIndex = currentCollectionKeys.indexOf(acceptedProps[j]);
+                if ( propIndex !== -1) {
+                    newObject[acceptedProps[j]] = propIndex
+                }
+            }
+            newCollection.push(newObject);
+
+        }
+    };
 }
 
 /**
  * @param {String} property – Свойство для фильтрации
  * @param {Array} values – Массив разрешённых значений
  */
+// var friends = [
+//     {
+//         name: 'Керри',
+//         gender: 'Женский',
+//         email: 'danamcgee@example.com',
+//         favoriteFruit: 'Апельсин'
+//     },
+//     {
+//         name: 'Стелла',
+//         gender: 'Женский',
+//         email: 'waltersguzman@example.com',
+//         favoriteFruit: 'Картофель'
+//     }
+// ];
+//     lib.filterIn('favoriteFruit', ['Яблоко', 'Картофель'])
 function filterIn(property, values) {
-    var filterObj = {};
-    filterObj[property] = values;
-    return filterObj;
+    // var filterObj = {};
+    // filterObj[property] = values;
+    return function filterIn(collection) {
+        var newCollection = [];
+        for (var i = 0; i < collection.length; i++) {
+            var currentCollectionKeys = Object.keys(collection[i]);
+            var valueIndex = currentCollectionKeys.indexOf(property);
+
+            if (valueIndex !== -1) {
+                var currentValue = collection[currentCollectionKeys[valueIndex]];
+
+                if(values.indexOf(currentValue) !== -1) {
+                    newCollection.push(JSON.parse(JSON.stringify(collection[i])))
+                }
+            }
+
+        }
+        return newCollection;
+    };
 }
 
 module.exports = {
